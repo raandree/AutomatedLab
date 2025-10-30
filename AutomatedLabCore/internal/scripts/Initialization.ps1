936ebb97450ca944d860002bf0318bf714bc2912
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2025-10-30 22:52:51 +0100

    Fix/1679 (#1791)
    * Include newer cmdlet to cache role sizes
    
    * Update nested HyperV role pattern
    
    * Update changelog

--- AutomatedLabCore/internal/scripts/Initialization.ps1
+++ AutomatedLabCore/internal/scripts/Initialization.ps1
@@ -1196,6 +1196,12 @@ Register-PSFTeppScriptblock -Name AutomatedLab-CustomRole -ScriptBlock {
     (Get-ChildItem -Path (Join-Path -Path (Get-LabSourcesLocationInternal -Local) -ChildPath 'CustomRoles' -ErrorAction SilentlyContinue) -Directory -ErrorAction SilentlyContinue).Name
 }
 
+Register-PSFTeppScriptblock -Name AutomatedLab-AzureRoleSize -ScriptBlock {
+    $defaultLocation = (Get-LabAzureDefaultLocation -ErrorAction SilentlyContinue).Location
+    (Get-AzComputeResourceSku -Location $defaultLocation -ErrorAction SilentlyContinue |
+    Where-Object ResourceType -eq virtualMachines | Sort-Object -Property Name).Name
+}
+
 Register-PSFTeppScriptblock -Name AutomatedLab-TimeZone -ScriptBlock {
     [System.TimeZoneInfo]::GetSystemTimeZones().Id | Sort-Object
 }
